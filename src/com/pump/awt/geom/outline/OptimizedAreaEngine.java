621b060208fd7ae49c0d255fff82a9ae4111d6a1
jeremy <jeremy.wood@mac.com>
2021-12-05 03:03:49 -0500

    Updating getBounds2D() method
    This implementation can provide a smaller/more accurate bounding box for quadratic/cubic segments.

--- src/com/pump/awt/geom/outline/OptimizedAreaEngine.java
+++ src/com/pump/awt/geom/outline/OptimizedAreaEngine.java
@@ -3,38 +3,218 @@ package com.pump.awt.geom.outline;
 import java.awt.*;
 import java.awt.geom.Area;
 import java.awt.geom.PathIterator;
+import java.awt.geom.QuadCurve2D;
 import java.awt.geom.Rectangle2D;
 import java.util.*;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class OptimizedAreaEngine implements OutlineEngine {
+
     /**
-     * TODO: remove this method if Path2D offers a static getBounds2D(PathIterator) method someday.
+     * Returns a high precision bounding box of the specified PathIterator.
+     * <p>
+     * This method provides a basic facility for implementors of the {@link Shape} interface to
+     * implement support for the {@link Shape#getBounds2D()} method.
+     * </p>
+     * <p>
+     * This is copied and pasted from a pull request for JDK 8176501
+     * (https://github.com/openjdk/jdk/pull/6227).
+     * </p>
+     *
+     * TODO: If the PR referenced above if accepted: remove this code and invoke the
+     * same method in Path2D instead. (Also remove the "accumulateX" methods below.)
+     *
+     * @return an instance of {@code Rectangle2D} that is a high-precision bounding box of the
+     *         {@code PathIterator}.
+     * @see Shape#getBounds2D()
      */
-    private static Rectangle2D getBounds2D(PathIterator pi) {
-        double[] coords = new double[6];
-        Rectangle2D r = null;
-        for(; !pi.isDone(); pi.next()) {
-            int k = pi.currentSegment(coords);
-            if (k == PathIterator.SEG_MOVETO) {
-                if (r == null) {
-                    r = new Rectangle2D.Double(coords[0], coords[1], 0, 0);
-                } else {
-                    r.add(coords[0], coords[1]);
+    public static Rectangle2D getBounds2D(final PathIterator pi) {
+        final double[] coeff = new double[4];
+        final double[] deriv_coeff = new double[3];
+
+        final double[] coords = new double[6];
+
+        // bounds are stored as {leftX, rightX, topY, bottomY}
+        double[] bounds = null;
+        double lastX = 0.0;
+        double lastY = 0.0;
+        double endX = 0.0;
+        double endY = 0.0;
+
+        for (; !pi.isDone(); pi.next()) {
+            final int type = pi.currentSegment(coords);
+            switch (type) {
+                case PathIterator.SEG_MOVETO:
+                    if (bounds == null) {
+                        bounds = new double[] { coords[0], coords[0], coords[1], coords[1] };
+                    }
+                    endX = coords[0];
+                    endY = coords[1];
+                    break;
+                case PathIterator.SEG_LINETO:
+                    endX = coords[0];
+                    endY = coords[1];
+                    break;
+                case PathIterator.SEG_QUADTO:
+                    endX = coords[2];
+                    endY = coords[3];
+                    break;
+                case PathIterator.SEG_CUBICTO:
+                    endX = coords[4];
+                    endY = coords[5];
+                    break;
+                case PathIterator.SEG_CLOSE:
+                default:
+                    continue;
+            }
+
+            if (endX < bounds[0]) bounds[0] = endX;
+            if (endX > bounds[1]) bounds[1] = endX;
+            if (endY < bounds[2]) bounds[2] = endY;
+            if (endY > bounds[3]) bounds[3] = endY;
+
+            switch (type) {
+                case PathIterator.SEG_QUADTO:
+                    accumulateExtremaBoundsForQuad(bounds, 0, lastX, coords[0], coords[2], coeff, deriv_coeff);
+                    accumulateExtremaBoundsForQuad(bounds, 2, lastY, coords[1], coords[3], coeff, deriv_coeff);
+                    break;
+                case PathIterator.SEG_CUBICTO:
+                    accumulateExtremaBoundsForCubic(bounds, 0, lastX, coords[0], coords[2], coords[4], coeff, deriv_coeff);
+                    accumulateExtremaBoundsForCubic(bounds, 2, lastY, coords[1], coords[3], coords[5], coeff, deriv_coeff);
+                    break;
+                default:
+                    break;
+            }
+
+            lastX = endX;
+            lastY = endY;
+        }
+        if (bounds != null) {
+            return new Rectangle2D.Double(bounds[0], bounds[2], bounds[1] - bounds[0], bounds[3] - bounds[2]);
+        }
+
+        // there's room to debate what should happen here, but historically we return a zeroed
+        // out rectangle here. So for backwards compatibility let's keep doing that:
+        return new Rectangle2D.Double();
+    }
+
+    /**
+     * Accumulate the quadratic extrema into the pre-existing bounding array.
+     * <p>
+     * This method focuses on one dimension at a time, so to get both the x and y
+     * dimensions you'll need to call this method twice.
+     * </p>
+     * <p>
+     * Whenever we have to examine cubic or quadratic extrema that change our bounding
+     * box: we run the risk of machine error that may produce a box that is slightly
+     * too small. But the contract of {@link Shape#getBounds2D()} says we should err
+     * on the side of being too large. So to address this: we'll apply a margin based
+     * on the upper limit of numerical error caused by the polynomial evaluation (horner
+     * scheme).
+     * </p>
+     *
+     * @param bounds the bounds to update, which are expressed as: { minX, maxX }
+     * @param boundsOffset the index in boundsof the minimum value
+     * @param x1 the starting value of the bezier curve where t = 0.0
+     * @param ctrlX the control value of the bezier curve
+     * @param x2 the ending value of the bezier curve where t = 1.0
+     * @param coeff an array of at least 3 elements that will be overwritten and reused
+     * @param deriv_coeff an array of at least 2 elements that will be overwritten and reused
+     */
+    private static void accumulateExtremaBoundsForQuad(double[] bounds, int boundsOffset, double x1, double ctrlX, double x2, double[] coeff, double[] deriv_coeff) {
+        if (ctrlX < bounds[boundsOffset] ||
+                ctrlX > bounds[boundsOffset + 1]) {
+
+            final double dx21 = ctrlX - x1;
+            coeff[2] = (x2 - ctrlX) - dx21;  // A = P3 - P0 - 2 P2
+            coeff[1] = 2.0 * dx21;           // B = 2 (P2 - P1)
+            coeff[0] = x1;                   // C = P1
+
+            deriv_coeff[0] = coeff[1];
+            deriv_coeff[1] = 2.0 * coeff[2];
+
+            final double t = -deriv_coeff[0] / deriv_coeff[1];
+            if (t > 0.0 && t < 1.0) {
+                final double v = coeff[0] + t * (coeff[1] + t * coeff[2]);
+
+                // error condition = sum ( abs (coeff) ):
+                final double margin = Math.ulp(Math.abs(coeff[0])
+                        + Math.abs(coeff[1]) + Math.abs(coeff[2]));
+
+                if (v - margin < bounds[boundsOffset]) {
+                    bounds[boundsOffset] = v - margin;
                 }
-            } else if (k == PathIterator.SEG_LINETO) {
-                r.add(coords[0], coords[1]);
-            } else if (k == PathIterator.SEG_QUADTO) {
-                r.add(coords[0], coords[1]);
-                r.add(coords[2], coords[3]);
-            } else if (k == PathIterator.SEG_CUBICTO) {
-                r.add(coords[0], coords[1]);
-                r.add(coords[2], coords[3]);
-                r.add(coords[4], coords[5]);
+                if (v + margin > bounds[boundsOffset + 1]) {
+                    bounds[boundsOffset + 1] = v + margin;
+                }
             }
         }
-        return r;
+    }
+
+    /**
+     * Accumulate the cubic extrema into the pre-existing bounding array.
+     * <p>
+     * This method focuses on one dimension at a time, so to get both the x and y
+     * dimensions you'll need to call this method twice.
+     * </p>
+     * <p>
+     * Whenever we have to examine cubic or quadratic extrema that change our bounding
+     * box: we run the risk of machine error that may produce a box that is slightly
+     * too small. But the contract of {@link Shape#getBounds2D()} says we should err
+     * on the side of being too large. So to address this: we'll apply a margin based
+     * on the upper limit of numerical error caused by the polynomial evaluation (horner
+     * scheme).
+     * </p>
+     *
+     * @param bounds the bounds to update, which are expressed as: { minX, maxX }
+     * @param boundsOffset the index in boundsof the minimum value
+     * @param x1 the starting value of the bezier curve where t = 0.0
+     * @param ctrlX1 the first control value of the bezier curve
+     * @param ctrlX1 the second control value of the bezier curve
+     * @param x2 the ending value of the bezier curve where t = 1.0
+     * @param coeff an array of at least 3 elements that will be overwritten and reused
+     * @param deriv_coeff an array of at least 2 elements that will be overwritten and reused
+     */
+    private static void accumulateExtremaBoundsForCubic(double[] bounds, int boundsOffset, double x1, double ctrlX1, double ctrlX2, double x2, double[] coeff, double[] deriv_coeff) {
+        if (ctrlX1 < bounds[boundsOffset] ||
+                ctrlX1 > bounds[boundsOffset + 1]) {
+            final double dx32 = 3.0 * (ctrlX2 - ctrlX1);
+            final double dx21 = 3.0 * (ctrlX1 - x1);
+            coeff[3] = (x2 - x1) - dx32;  // A = P3 - P0 - 3 (P2 - P1) = (P3 - P0) + 3 (P1 - P2)
+            coeff[2] = (dx32 - dx21);         // B = 3 (P2 - P1) - 3(P1 - P0) = 3 (P2 + P0) - 6 P1
+            coeff[1] = dx21;                  // C = 3 (P1 - P0)
+            coeff[0] = x1;                 // D = P0
+
+            deriv_coeff[0] = coeff[1];
+            deriv_coeff[1] = 2.0 * coeff[2];
+            deriv_coeff[2] = 3.0 * coeff[3];
+
+            // reuse this array, give it a new name for readability:
+            final double[] tExtrema = deriv_coeff;
+
+            // solveQuadratic should be improved to get correct t extrema (1 ulp):
+            final int tExtremaCount = QuadCurve2D.solveQuadratic(deriv_coeff, tExtrema);
+            if (tExtremaCount > 0) {
+                // error condition = sum ( abs (coeff) ):
+                final double margin = Math.ulp(Math.abs(coeff[0])
+                        + Math.abs(coeff[1]) + Math.abs(coeff[2])
+                        + Math.abs(coeff[3]));
+
+                for (int i = 0; i < tExtremaCount; i++) {
+                    final double t = tExtrema[i];
+                    if (t > 0.0 && t < 1.0) {
+                        final double v = coeff[0] + t * (coeff[1] + t * (coeff[2] + t * coeff[3]));
+                        if (v - margin < bounds[boundsOffset]) {
+                            bounds[boundsOffset] = v - margin;
+                        }
+                        if (v + margin > bounds[boundsOffset + 1]) {
+                            bounds[boundsOffset + 1] = v + margin;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
