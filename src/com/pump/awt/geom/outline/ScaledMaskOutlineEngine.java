fa4f2a5cce273b9c0ef9777ba665c44bbee3b882
jeremy <jeremy.wood@mac.com>
2021-12-27 01:21:30 -0500

    Adding ScaledMaskOutlineEngineTests#testReuse
    This includes a rough first draft of a solution that satisfies the unit test. (I'll probably keep tweaking it, though.)

--- src/com/pump/awt/geom/outline/ScaledMaskOutlineEngine.java
+++ src/com/pump/awt/geom/outline/ScaledMaskOutlineEngine.java
@@ -1,9 +1,10 @@
 package com.pump.awt.geom.outline;
 
+import com.pump.awt.geom.ShapeUtils;
 import com.pump.awt.geom.mask.RectangleMask;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
+import java.awt.geom.*;
 import java.util.List;
 
 /**
@@ -36,7 +37,19 @@ public class ScaledMaskOutlineEngine implements OutlineEngine {
     public Shape calculate(List<OutlineOperation> operations) {
         RectangleMask returnValue = new RectangleMask();
         for(OutlineOperation op : operations) {
-            RectangleMask operand = new RectangleMask(op.shape, scaleUp, 1);
+            RectangleMask operand;
+            Shape shape = op.shape;
+            if (shape instanceof ScaledShape) {
+                ScaledShape ss = (ScaledShape) shape;
+                if (ss.getScale() == 1.0 / resolution)
+                    shape = ((ScaledShape)shape).getShape();
+            }
+            if (shape instanceof RectangleMask) {
+                operand = (RectangleMask) shape;
+            } else {
+                operand = new RectangleMask(op.shape, scaleUp, 1);
+            }
+
             switch (op.type) {
                 case ADD:
                     returnValue.add(operand);
@@ -52,7 +65,8 @@ public class ScaledMaskOutlineEngine implements OutlineEngine {
                     break;
             }
         }
-        return scaleDown.createTransformedShape(returnValue);
+//        return scaleDown.createTransformedShape(returnValue);
+        return new ScaledShape(returnValue, 1.0 / resolution);
     }
 
     @Override
@@ -66,5 +80,80 @@ public class ScaledMaskOutlineEngine implements OutlineEngine {
         if (resolution == 1)
             return str;
         return str + "[ resolution = "+resolution+"]";
+    }
+
+    static class ScaledShape implements Shape {
+
+        protected final Shape delegate;
+        protected final double scale;
+
+        public ScaledShape(Shape shape, double scale) {
+            this.delegate = shape;
+            this.scale = scale;
+        }
+
+        @Override
+        public Rectangle getBounds() {
+            return getBounds2D().getBounds();
+        }
+
+        @Override
+        public Rectangle2D getBounds2D() {
+            return ShapeUtils.getBounds2D(delegate.getPathIterator(AffineTransform.getScaleInstance(scale, scale)));
+        }
+
+        @Override
+        public boolean contains(double x, double y) {
+            return delegate.contains(x / scale, y / scale);
+        }
+
+        @Override
+        public boolean contains(Point2D p) {
+            return contains(p.getX(), p.getY());
+        }
+
+        @Override
+        public boolean intersects(double x, double y, double w, double h) {
+            return delegate.intersects(x / scale, y / scale, w / scale, h / scale);
+        }
+
+        @Override
+        public boolean intersects(Rectangle2D r) {
+            return intersects(r.getX(), r.getY(), r.getWidth(), r.getHeight());
+        }
+
+        @Override
+        public boolean contains(double x, double y, double w, double h) {
+            return delegate.contains(x / scale, y / scale, w / scale, h / scale);
+        }
+
+        @Override
+        public boolean contains(Rectangle2D r) {
+            return contains(r.getX(), r.getY(), r.getWidth(), r.getHeight());
+        }
+
+        @Override
+        public PathIterator getPathIterator(AffineTransform at) {
+            if (at == null)
+                at = new AffineTransform();
+            at.concatenate(AffineTransform.getScaleInstance(scale, scale));
+            return delegate.getPathIterator(at);
+        }
+
+        @Override
+        public PathIterator getPathIterator(AffineTransform at, double flatness) {
+            if (at == null)
+                at = new AffineTransform();
+            at.concatenate(AffineTransform.getScaleInstance(scale, scale));
+            return delegate.getPathIterator(at, flatness);
+        }
+
+        public Shape getShape() {
+            return delegate;
+        }
+
+        public double getScale() {
+            return scale;
+        }
     }
 }
