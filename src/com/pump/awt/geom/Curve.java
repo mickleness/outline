182b98f9dc11a0b1d46de9ca9da077d222c0b5a5
jeremy <jeremy.wood@mac.com>
2024-06-05 01:06:09 -0400

    Apply new logic to recursive `rectCrossingsForX` also

--- src/com/pump/awt/geom/Curve.java
+++ src/com/pump/awt/geom/Curve.java
@@ -210,20 +210,21 @@ public abstract class Curve {
                     crossings = RECT_INTERSECTS;
                     return;
                 }
-
-                double curveMinX = Math.min(Math.min(x0, x1), xc);
-                double curveMaxX = Math.max(Math.max(x0, x1), xc);
-                double curveMinY = Math.min(Math.min(y0, y1), yc);
-                double curveMaxY = Math.max(Math.max(y0, y1), yc);
-                if (rymin < curveMinY && rymax > curveMaxY && rxmin > curveMinX && rxmax < curveMaxX) {
-                    crossings = RECT_INTERSECTS;
-                    return;
-                }
-                if (rxmin < curveMinX && rxmax > curveMaxX && rymin > curveMinY && rymax < curveMaxY) {
-                    crossings = RECT_INTERSECTS;
-                    return;
-                }
             }
+
+            double curveMinX = Math.min(Math.min(x0, x1), xc);
+            double curveMaxX = Math.max(Math.max(x0, x1), xc);
+            double curveMinY = Math.min(Math.min(y0, y1), yc);
+            double curveMaxY = Math.max(Math.max(y0, y1), yc);
+            if (rymin < curveMinY && rymax > curveMaxY && rxmin > curveMinX && rxmax < curveMaxX) {
+                crossings = RECT_INTERSECTS;
+                return;
+            }
+            if (rxmin < curveMinX && rxmax > curveMaxX && rymin > curveMinY && rymax < curveMaxY) {
+                crossings = RECT_INTERSECTS;
+                return;
+            }
+
             // Otherwise, subdivide and look for one of the cases above.
             // double precision only has 52 bits of mantissa
             if (level > 52) {
@@ -305,25 +306,25 @@ public abstract class Curve {
                 // First do trivial INTERSECTS rejection of the cases
                 // where one of the endpoints is inside the rectangle.
                 if ((x0 > rxmin && x0 < rxmax && y0 > rymin && y0 < rymax) ||
-                        (x1 > rxmin && x1 < rxmax && y1 > rymin && y1 < rymax))
-                {
-                    crossings = RECT_INTERSECTS;
-                    return;
-                }
-
-                double curveMinX = Math.min(Math.min(x0, xc0), Math.min(xc1, x1));
-                double curveMaxX = Math.max(Math.max(x0, xc0), Math.max(xc1, x1));
-                double curveMinY = Math.min(Math.min(y0, yc0), Math.min(yc1, x1));
-                double curveMaxY = Math.max(Math.max(y0, yc0), Math.max(yc1, x1));
-                if (rymin < curveMinY && rymax > curveMaxY && rxmin > curveMinX && rxmax < curveMaxX) {
-                    crossings = RECT_INTERSECTS;
-                    return;
-                }
-                if (rxmin < curveMinX && rxmax > curveMaxX && rymin > curveMinY && rymax < curveMaxY) {
+                        (x1 > rxmin && x1 < rxmax && y1 > rymin && y1 < rymax)) {
                     crossings = RECT_INTERSECTS;
                     return;
                 }
             }
+
+            double curveMinX = Math.min(Math.min(x0, xc0), Math.min(xc1, x1));
+            double curveMaxX = Math.max(Math.max(x0, xc0), Math.max(xc1, x1));
+            double curveMinY = Math.min(Math.min(y0, yc0), Math.min(yc1, x1));
+            double curveMaxY = Math.max(Math.max(y0, yc0), Math.max(yc1, x1));
+            if (rymin < curveMinY && rymax > curveMaxY && rxmin > curveMinX && rxmax < curveMaxX) {
+                crossings = RECT_INTERSECTS;
+                return;
+            }
+            if (rxmin < curveMinX && rxmax > curveMaxX && rymin > curveMinY && rymax < curveMaxY) {
+                crossings = RECT_INTERSECTS;
+                return;
+            }
+
             // Otherwise, subdivide and look for one of the cases above.
             // double precision only has 52 bits of mantissa
             if (level > 52) {
--- tests/com/pump/awt/geom/CurveTest.java
+++ tests/com/pump/awt/geom/CurveTest.java
@@ -1,6 +1,7 @@
 package com.pump.awt.geom;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.awt.geom.CubicCurve2D;
 import java.awt.geom.QuadCurve2D;
@@ -15,6 +16,7 @@ public class CurveTest extends TestCase {
      * <p>
      * This is testing efficiency; this is not testing correctness.
      */
+    @Test
     public void testCrossingCounter() {
         AtomicInteger maxRecursionLevel = new AtomicInteger(0);
 
@@ -90,4 +92,79 @@ public class CurveTest extends TestCase {
         assertEquals(0, maxRecursionLevel.get());
     }
 
+    @Test
+    public void testCrossingCounter_var2() {
+        AtomicInteger maxRecursionLevel = new AtomicInteger(0);
+
+        CubicCurve2D cubicCurve = new CubicCurve2D.Double(0,0,0,50,0,100,100,100);
+
+        // test against a skinny vertical rectangle that extends above and below our curve
+        new Curve.RectCrossingsCounter(cubicCurve.getPathIterator(null), 67.001, 80, 67.002, 110) {
+
+            @Override
+            void rectCrossingsForCubic(double x0,  double y0,
+                                       double xc0, double yc0,
+                                       double xc1, double yc1,
+                                       double x1,  double y1,
+                                       int level) {
+                maxRecursionLevel.set(Math.max(level, maxRecursionLevel.get()));
+                super.rectCrossingsForCubic(x0, y0, xc0, yc0, xc1, yc1, x1, y1, level);
+            }
+
+        };
+
+        // this example reached a maxRecursionLevel of 17 before this change
+        assertEquals(2, maxRecursionLevel.get());
+
+        // test against a skinny horizontal rectangle that extends past the left and right edge of our curve
+        new Curve.RectCrossingsCounter(cubicCurve.getPathIterator(null), -10, 53, 33, 54) {
+
+            @Override
+            void rectCrossingsForCubic(double x0,  double y0,
+                                       double xc0, double yc0,
+                                       double xc1, double yc1,
+                                       double x1,  double y1,
+                                       int level) {
+                maxRecursionLevel.set(Math.max(level, maxRecursionLevel.get()));
+                super.rectCrossingsForCubic(x0, y0, xc0, yc0, xc1, yc1, x1, y1, level);
+            }
+
+        };
+        assertEquals(2, maxRecursionLevel.get());
+
+
+        QuadCurve2D quadCurve2D = new QuadCurve2D.Double(0,0,0,100,100,100);
+
+        // test against a skinny vertical rectangle that extends above and below our curve
+        new Curve.RectCrossingsCounter(quadCurve2D.getPathIterator(null), 53, 80, 54, 110) {
+
+            @Override
+            void rectCrossingsForCubic(double x0,  double y0,
+                                       double xc0, double yc0,
+                                       double xc1, double yc1,
+                                       double x1,  double y1,
+                                       int level) {
+                maxRecursionLevel.set(Math.max(level, maxRecursionLevel.get()));
+                super.rectCrossingsForCubic(x0, y0, xc0, yc0, xc1, yc1, x1, y1, level);
+            }
+
+        };
+        assertEquals(2, maxRecursionLevel.get());
+
+        // test against a skinny horizontal rectangle that extends past the left and right edge of our curve
+        new Curve.RectCrossingsCounter(quadCurve2D.getPathIterator(null), -10, 53, 33, 54) {
+
+            @Override
+            void rectCrossingsForCubic(double x0,  double y0,
+                                       double xc0, double yc0,
+                                       double xc1, double yc1,
+                                       double x1,  double y1,
+                                       int level) {
+                maxRecursionLevel.set(Math.max(level, maxRecursionLevel.get()));
+                super.rectCrossingsForCubic(x0, y0, xc0, yc0, xc1, yc1, x1, y1, level);
+            }
+
+        };
+        assertEquals(2, maxRecursionLevel.get());
+    }
 }
